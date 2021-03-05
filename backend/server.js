01b1bcf6f94d59e3eb9502e36a77814ff826a47b
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-06 00:34:51 +0100

    updated backend

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = require('./app')
 const dotenv = require('dotenv');
 
 // setting up config file
-dotenv.config({pat:'backend/config/config.env'})
+dotenv.config({path:'backend/config/config.env'})
 
 app.listen(process.env.PORT, () => {
     console.log(`server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
